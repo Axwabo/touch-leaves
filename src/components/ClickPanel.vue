e045496b6b54bce7e5ec9ad12197c49b2efbb23c
Axwabo <axwabo@gmail.com>
2025-10-25 13:02:39 +0200

    improve theme

--- src/components/ClickPanel.vue
+++ src/components/ClickPanel.vue
@@ -5,15 +5,17 @@ const { touch } = useStore();
 </script>
 
 <template>
-    <div id="touch" v-on:click="touch(1)">
+    <button id="touch" v-on:click="touch(1)">
         <span class="leaf">🍁</span>
-        <h1>touch<br>leaves</h1>
+        <span class="title">touch<br>leaves</span>
         <span class="leaf">🍂</span>
-    </div>
+    </button>
 </template>
 
 <style scoped>
 #touch {
+    background-color: transparent;
+    border: none;
     cursor: pointer;
     display: flex;
     align-items: center;
@@ -25,5 +27,10 @@ const { touch } = useStore();
 .leaf {
     line-height: 0;
     font-size: 5em;
+}
+
+.title {
+    font-weight: bold;
+    font-size: 5rem;
 }
 </style>
