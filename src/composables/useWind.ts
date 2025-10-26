5b3b615d6d6c4eeaa4f685db879e7f97f57fdd11
Axwabo <axwabo@gmail.com>
2025-10-26 15:09:26 +0100

    scale wind particles

--- src/composables/useWind.ts
+++ src/composables/useWind.ts
@@ -1,10 +1,15 @@
 import { onMounted, type Ref } from "vue";
 import useInterval from "./useInterval.ts";
 
-const keyframes = [ { opacity: 0 }, { opacity: 1 }, { opacity: 1 }, { opacity: 0, translate: "10rem 2rem" } ];
+const keyframes = [
+    { opacity: 0, scale: 0 },
+    { opacity: 1, scale: 1 },
+    { opacity: 1, scale: 1 },
+    { opacity: 0, scale: 0, translate: "10rem 2rem" }
+];
 
 export default function useWind(ref: Ref<HTMLElement | null>) {
-    useInterval(() => ref.value?.animate(keyframes, { duration: 1000 }), Math.random() * 3000 + 3000);
+    useInterval(() => ref.value?.animate(keyframes, { duration: 1000 }), Math.random() * 3000 + 2000);
     if (Math.random() < 0.8)
         onMounted(() => ref.value?.animate(keyframes, { duration: 1000, delay: Math.random() * 2000 }));
 }
