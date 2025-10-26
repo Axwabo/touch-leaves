e7dd3888bda09b7a74d549ddec908729a740591f
Axwabo <axwabo@gmail.com>
2025-10-26 10:27:59 +0100

    new leaf sounds

--- src/utils/sources.ts
+++ src/utils/sources.ts
@@ -1,8 +1,11 @@
 import leaf1 from "../assets/audio/leaf1.ogg";
 import leaf2 from "../assets/audio/leaf2.ogg";
+import leaf3 from "../assets/audio/leaf3.ogg";
+import leaf4 from "../assets/audio/leaf4.ogg";
+import leaf5 from "../assets/audio/leaf5.ogg";
 import { item } from "./random.ts";
 
-const leaves = [ leaf1, leaf2 ];
+const leaves = [ leaf1, leaf2, leaf3, leaf4, leaf5 ];
 
 export function playLeafSound() {
     const container = document.getElementById("leafSoundContainer");
